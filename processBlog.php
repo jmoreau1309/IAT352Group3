8381e9da756d79cf448092c716f984eba1768b8f
Kybre <hermanchan10@gmail.com>
2022-11-29 07:39:23 -0800

    implemented writeBlog.php and added scaffold for processBlog.php

---
+++ processBlog.php
@@ -0,0 +1,13 @@
+<?php
+ include('./assets/functions.php');
+ no_SSL();
+?>
+<html>
+  <?php include('header.php'); ?>
+  <head>
+  </head>
+  <body>
+    <div class="content">
+    </div>
+  </body>
+</html>
---
+++ writeBlog.php
@@ -0,0 +1,46 @@
+<?php
+ include('./assets/functions.php');
+ require('./data/db.php');
+ no_SSL();
+?>
+<html>
+  <?php
+    include('header.php');
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); //connect to db
+    if(mysqli_connect_errno()) die(mysqli_connect_error()); //test for successful connection
+
+    if(!isset($_POST["artID"])) header("Location: showArt.php"); //return to show art page if user access this page without a piece to blog about
+    if(!isset($_SESSION["user"])) header("Location: sign-in.php"); //redirect to sign in if user isn't logged in
+  ?>
+  <head>
+  </head>
+  <body>
+    <div class="content">
+      <!--placeholder for form to fill out-->
+      <h1> Write Blog </h1>
+      <h2>Your Selected Art Piece:</h2>
+
+      <?php
+        $art_query = "SELECT artpieces.title, artpieces.filename FROM artpieces WHERE art_id=".$_POST["artID"];
+        $art_result = mysqli_query($connection, $art_query);
+        if (!$art_result) die("Database query failed."); //test for error
+
+        if(mysqli_num_rows($art_result) != 0){
+          while($r=mysqli_fetch_assoc($art_result)){
+            echo "<img src=\"./assets/img/".$r["filename"]."\" class=\"display-img\"/><br/><br/>";
+            echo "<i>".$r["title"]."</i><br/>";
+          }
+        }
+      ?>
+
+      <form method="get" action="processBlog.php">
+        <input type="hidden" name="artID" value="<?php echo $_POST["artID"]; //art piece id ?>"/>
+        <h2 style="margin-bottom: 0;">Title:</h2>
+        <input type="text" name="title" value=""/>
+        <h2 style="margin-bottom: 0;">Content:</h2>
+        <textarea name="content" cols="80" rows="10" placeholder="Type out your blog content here!"></textarea><br/><br/>
+        <input type="submit" value="Submit Blog"/>
+      </form>
+    </div>
+  </body>
+</html>
