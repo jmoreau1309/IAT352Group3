2031fa600785ac9c0417366335e00226e6f2a656
Kybre <hermanchan10@gmail.com>
2022-11-29 07:33:23 -0800

    comments to indicate reused code

--- register.php
+++ register.php
@@ -6,6 +6,7 @@
 ?>
 <html>
   <?php
+    //code largely recycled from Herman's A4
     $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); //connect to db
     if(mysqli_connect_errno()) die(mysqli_connect_error()); //test for successful connection
 
