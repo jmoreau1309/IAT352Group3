f3b0a82a5ead76f7f60751947b1b88558772371e
Jennifer Moreau <jennifer_moreau@sfu.ca>
2022-12-06 22:41:17 -0800

    Added Style in CSS and Working on Home Display

--- index.php
+++ index.php
@@ -6,14 +6,17 @@
   <?php
     include('header.php');
     $_SESSION["return_to_url"] = $_SERVER['REQUEST_URI'];
+
   ?>
   <head>
     <title>Art Blogger</title>
     <link href="./CSS/main.css" rel="stylesheet">
   </head>
   <body>
-    <div class="content">
-      <!--Featured Art/Blog?-->
+    <div class="content padding" style="max-width:1564px">
+      <div class="container padding-32" id="home">
+    <h2 class="border-bottom border-light-blue padding-16">Your Latest Activity</h2>
+      </div>
     </div>
   </body>
 </html>
