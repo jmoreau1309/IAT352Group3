cbfc2f8e454f236a9193c333cb7db271d4fb9c4c
Kybre <hermanchan10@gmail.com>
2022-12-07 04:58:02 -0800

    updated filter functionality such that values are saved across pages

--- assets/js/showArt.js
+++ assets/js/showArt.js
@@ -1,14 +1,10 @@
-$(document).ready(function() {
+window.onload = function(){
   filterGenres($('#select_genre').val());
 
   // filter on select change
   $('#select_genre').on("change", function(){
     filterGenres($(this).val());
   });
-});
-
-window.onload = function(){
-  $('select').prop('selectedIndex', 0);
 };
 
 function filterGenres(value){
@@ -27,6 +23,8 @@ function filterGenres(value){
       $(".art-list").empty() //remove all current elements
 
       result.forEach(element => {
+          //assign entries into array and create for statement that filters by 10s based on GET paremeter
+          //shows default first page with no GET parameter, also works with GET['page']=1;
           $(".art-list").append(`
           <li>
               <a href=\"artdetails.php?artID=${element.art_id}\">${element.title}</a>
